cd941dd8b043d149257127dce2659813ade9876a
Viacheslav Danylko <gamerdvv@gmail.com>
2024-10-28 12:23:27 +0200

    add code to files app and main

---
+++ src/components/App.jsx
@@ -0,0 +1,24 @@
+import Product from "./product";
+
+export default function App() {
+  return (
+    <div>
+      <h1>Best selling</h1>
+
+      <Product
+        name="Tacos With Lime"
+        imgUrl="https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640"
+        price={10.99}
+      />
+      <Product
+        name="Fries and Burger"
+        imgUrl="https://images.pexels.com/photos/70497/pexels-photo-70497.jpeg?dpr=2&h=480&w=640"
+        price={14.29}
+      />
+      <Product
+        name="Coca Cola"
+        price={1.99}
+      />
+    </div>
+  );
+}
